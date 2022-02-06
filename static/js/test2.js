1e83cfbc69c69b591454c3ea89403b0a33cdab0a
gram89 <89284806+gram89@users.noreply.github.com>
2022-02-06 11:46:52 -0500

    Add files via upload

--- static/js/test2.js
+++ static/js/test2.js
@@ -38,11 +38,18 @@ function optionChanged(newSample) {
 
   function buildMetadata(city) {
     d3.csv("static/resources/clean_historical.csv").then(function(data){
-        // 3. Create a variable that holds the samples array. 
+        // 3. Create a variable that holds the filtered array
         let filteredData = data.filter(a => a.City == city);
-      // Filter the data for the object with the desired sample number
-      //var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-      var result = filteredData[0];
+        var filteredColumns = filteredData.map(function(d) {
+          return {
+            "Average Sold Price($)": d.Sold_price_avg,
+            "Average List Price($)": d.List_price_avg,
+            "Days on the Market": d.days_on_mar,
+
+          }
+        });
+        
+      var result = filteredColumns[0];
       // Use d3 to select the panel with id of `#sample-metadata`
       var PANEL = d3.select("#sample-metadata");
   
@@ -53,14 +60,14 @@ function optionChanged(newSample) {
       // Hint: Inside the loop, you will need to use d3 to append new
       // tags for each key-value in the metadata.
       Object.entries(result).forEach(([key, value]) => {
-        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        PANEL.append("h3").text(`${key.toUpperCase()}: ${value}`);
       });
   
     });
   }
 
   function buildCharts(city) {
-    // 2. Use d3.json to load and retrieve the samples.json file 
+    // 2. Use d3.json to load and retrieve the data
     d3.csv("static/resources/clean_historical.csv").then(function(data){
       // 3. Create a variable that holds the samples array. 
       let filteredData = data.filter(a => a.City == city);
@@ -74,8 +81,14 @@ function optionChanged(newSample) {
       let sold2021 = filter2021.map(a =>parseFloat(a.Sold_price_avg));
       //let list2021 = filter2021.map(a =>parseFloat(a.List_price_avg));
       let listVsSold2021 = filter2021.map(a =>(parseFloat(a.Sold_price_avg)-parseFloat(a.List_price_avg)));
+      let listVsSold2020 = filter2020.map(a =>(parseFloat(a.Sold_price_avg)-parseFloat(a.List_price_avg)));
+      let listVsSold2019 = filter2019.map(a =>(parseFloat(a.Sold_price_avg)-parseFloat(a.List_price_avg)));
+      let listVsSold2018 = filter2018.map(a =>(parseFloat(a.Sold_price_avg)-parseFloat(a.List_price_avg)));
+      let listVsSold2017 = filter2017.map(a =>(parseFloat(a.Sold_price_avg)-parseFloat(a.List_price_avg)));
+      let days2021 = filter2021.map(a =>parseFloat(a.days_on_mar));
+      let days2020 = filter2020.map(a =>parseFloat(a.days_on_mar));
+      let days2019 = filter2019.map(a =>parseFloat(a.days_on_mar));
       console.log(month2021.reverse());
-
       //console.log(monthArray);
       var lineData2021 = {
         x: month2021.reverse(),
@@ -84,19 +97,28 @@ function optionChanged(newSample) {
         type: "scatter",
         name: "2021"
         //orientation: "h"
-      };
+      }
       
       var barData2021 ={
           x: month2021.reverse(),
           y: listVsSold2021.reverse(),
           type: "bar",
+          name: "2021"
 
       }
+
+      var daysonMar2021 ={
+        x: month2021.reverse(),
+        y: days2021.reverse(),
+        type: "scatter",
+        name: "2021"
+
+    }
       
       let month2020 = filter2020.map(a =>a.Month);
       let sold2020 = filter2020.map(a =>parseFloat(a.Sold_price_avg));
       //let list2020 = filter2020.map(a =>parseFloat(a.List_price_avg));
-      let listVsSold2020 = filter2020.map(a =>(parseFloat(a.Sold_price_avg)-parseFloat(a.List_price_avg)));
+      //let listVsSold2020 = filter2020.map(a =>(parseFloat(a.Sold_price_avg)-parseFloat(a.List_price_avg)));
 
       //console.log(monthArray);
       var lineData2020 = {
@@ -107,9 +129,22 @@ function optionChanged(newSample) {
         name: "2020"
         //orientation: "h"
       }
-        
-      ;
 
+      var barData2020 ={
+        x: month2020.reverse(),
+        y: listVsSold2020.reverse(),
+        type: "bar",
+        name: "2020"
+
+    }
+    var daysonMar2020 ={
+      x: month2020.reverse(),
+      y: days2020.reverse(),
+      type: "scatter",
+      name: "2020"
+
+    }
+    
       let month2019 = filter2019.map(a =>a.Month);
       let sold2019 = filter2019.map(a =>parseFloat(a.Sold_price_avg));
 
@@ -121,7 +156,22 @@ function optionChanged(newSample) {
         type: "scatter",
         name: "2019"
         //orientation: "h"
-      };
+      }
+
+      var barData2019 ={
+        x: month2019.reverse(),
+        y: listVsSold2019.reverse(),
+        type: "bar",
+        name: "2019"
+
+    }
+    var daysonMar2019 ={
+      x: month2019.reverse(),
+      y: days2019.reverse(),
+      type: "scatter",
+      name: "2019"
+
+  }
       let month2018 = filter2018.map(a =>a.Month);
       let sold2018 = filter2018.map(a =>parseFloat(a.Sold_price_avg));
 
@@ -133,7 +183,15 @@ function optionChanged(newSample) {
         type: "scatter",
         name: "2018"
         //orientation: "h"
-      };
+      }
+
+      var barData2018 ={
+        x: month2018.reverse(),
+        y: listVsSold2018.reverse(),
+        type: "bar",
+        name: "2018"
+
+    }
       let month2017 = filter2017.map(a =>a.Month);
       let sold2017 = filter2017.map(a =>parseFloat(a.Sold_price_avg));
 
@@ -145,46 +203,73 @@ function optionChanged(newSample) {
         type: "scatter",
         name: "2017"
         //orientation: "h"
-      };
+      }
 
-      var dataSoldPrice = [ lineData2021, lineData2020, lineData2019, lineData2018, lineData2017]
+      var barData2017 ={
+        x: month2017.reverse(),
+        y: listVsSold2017.reverse(),
+        type: "bar",
+        name: "2017"
+
+    }
+
+      var dataSoldPrice = [ lineData2021, lineData2020, lineData2019, lineData2018, lineData2017];
+      var soldVsList = [barData2021, barData2020, barData2019];
+      var daysOnMar = [daysonMar2021, daysonMar2020, daysonMar2019];
       // 9. Create the layout for the bar chart. 
       var lineLayout = {
         title: "Sold Price Past 5 years"
              
       };
       var barLayout = {
-        title: "How high are are people bidding"
+        title: "How high are are people bidding over the list price?",
+        subtitle: "Difference between Sold and List Prices"
              
       };
+
+      var soldDays = {
+        title: "How long does a home stay on the market before selling?",
+        subtitle: "Difference between Sold and List Prices"
+             
+      };
+
       // 10. Use Plotly to plot the data with the layout. 
       Plotly.newPlot("bar",dataSoldPrice,lineLayout)
 
-      Plotly.newPlot("gauge",[barData2021],barLayout)
-      //let listArray = data.map(a =>a.List_price_avg);
-      //let soldListDiff = parseFloat(data.map(a =>a.Sold_price_avg)) - parseFloat(data.map(a =>a.List_price_avg));
-      //let daysArray = data.map(a =>a.days_on_mar);
+      Plotly.newPlot("gauge",soldVsList,barLayout)
 
-    //   // 4. Create a variable that filters the samples for the object with the desired sample number.
-    //       let filteredMonth = month.filter(d => d.City == sample);
-    //       let filteredYear = year.filter(d => d.City == sample);
-    //       let filteredSold = sold.filter(d => d.City == sample);
-    //       let filteredList = list.filter(d => d.City == sample);
-    //       let filteredDays = days.filter(d => d.City == sample);
-
-    //       console.log(filteredMonth[0]);
+      Plotly.newPlot("bibble1", daysOnMar, soldDays)
+    
     })
 }
 
 function buildListings(city) {
   d3.csv("static/resources/clustered_listings.csv").then(function(data){
+    // 3. Create a variable that holds the filtered array
+    let filteredData = data.filter(a => a.City.toUpperCase() == city.toUpperCase());
+    var filteredColumns = filteredData.map(function(d) {
+      return {
+        "Listing Address": d.Address,
+        "Postal Code": d.Postal_Code,
+        "Beds": d.Beds,
+        "Baths": d.Baths,
+        "List Price": d.Price
+
+      }
+    });
+    
       // 3. Create a variable that holds the samples array. 
-      let filteredData = data.filter(a => a.City.toUpperCase() == city.toUpperCase());
+    //  let filteredColumns = data.filter(a => a.City.toUpperCase() == city.toUpperCase());
     // Filter the data for the object with the desired sample number
     //var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = filteredData[0];
+    var result1 = filteredColumns[0];
+    var result2 = filteredColumns[1];
+    var result3=filteredColumns[2];
+    var result4 = filteredColumns[3];
+    var result5 = filteredColumns[4];
+    console.log(filteredData[0]);
     // Use d3 to select the panel with id of `#sample-metadata`
-    var PANEL = d3.select("#listings");
+    var PANEL = d3.select("#listing1");
 
     // Use `.html("") to clear any existing metadata
     PANEL.html("");
@@ -192,8 +277,20 @@ function buildListings(city) {
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-    Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    Object.entries(result1).forEach(([key, value]) => {
+      PANEL.append("h3").text(`${key.toUpperCase()}: ${value}`);
+    });
+
+    var PANEL = d3.select("#listing2");
+
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(result2).forEach(([key, value]) => {
+      PANEL.append("h3").text(`${key.toUpperCase()}: ${value}`);
     });
 
   });
